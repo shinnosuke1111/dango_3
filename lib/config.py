a799435a7b3a014d70e32e60cdb6a850df7cb55a
toh981022 <2023jfr-ic-student002@la-study.com>
2023-06-08 13:19:03 +0900

    zunda

--- lib/config.py
+++ lib/config.py
@@ -3,7 +3,9 @@ import os
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/mitarashi_dango'.format(**{
     'user': os.getenv('DB_USER', 'postgres'),
     'password': os.getenv('DB_PASSWORD', 'himitu'),
-    'host': os.getenv('DB_HOST', 'localhost:5432'),
+    'host': os.getenv('DB_HOST', 'localhost:5432'
+    
+    ),
 })
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
