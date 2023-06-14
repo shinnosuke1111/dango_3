4da944601707b28ae0364f76fb2e2980a07c53ab
shinnosuke1111 <2023jfr-ic-student001@la-study.com>
2023-06-14 15:29:40 +0900

    仮完成

--- user/views.py
+++ user/views.py
@@ -1,40 +1,24 @@
 # flask関連のパッケージを取得
 from flask import render_template, request, url_for, session, redirect, flash
-
-
 # 「__init__.py」で宣言した変数appを取得
 from user import app
-
-
 # Itemモデルを取得
 from lib.models import Account, Basic_information
-
 import os
-
 # import flask_login
-
-
 # SQLAlchemyを取得
 from lib.db import db
-
-
 # デコレーターに使用
 from functools import wraps
-
-
 # ログインチェック処理
 def login_check(view):
   @wraps(view)
   def inner(*args, **kwargs):
     if not session.get('logged_in'):
       flash('ログインしてください', 'error')
-      return redirect(url_for('user_login'))    
+      return redirect(url_for('user_login'))
     return view(*args, **kwargs)
   return inner
-
-
-
-
 # ログイン
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
@@ -45,20 +29,19 @@ def user_login():
     try:
       account = Account.query.filter_by(email=email).first()
       if account.password == password:
-        login_user(account) 
-        print("login")
+        session['logged_in'] = True
+        session['account_id'] = account.account_id
         flash('ログインしました', 'success')
         print("login")
         return redirect(url_for('user_index'))
     except:
       flash('正しいメールアドレスとパスワードを入力して下さい。')
-      return "ok"
-      # return render_template('login.html')
+      return render_template('login.html')
       # get_flashed_messages()
-  return render_template('users/top.html')
-
-
-
+  elif not session.get('logged_in'):
+    return render_template('login.html')
+  else:
+    return redirect(url_for('user_index'))
 
 # ログアウト
 @app.route('/logout')
@@ -67,14 +50,12 @@ def user_logout():
   flash('ログアウトしました', 'success')
   return render_template('login.html')
 
-
 # 従業員一覧(アカウント情報)を表示
 @app.route('/users')
 @login_check
 def user_index():
   accounts = Account.query.order_by(Account.account_id.desc()).all()
   return render_template('users/top.html', accounts=accounts)
-
 
 # 従業員詳細(基本情報)を表示
 @app.route('/users/<int:account_id>')
@@ -84,12 +65,10 @@ def user_show(account_id):
   basic_information = Basic_information.query.get(account_id)
   return render_template('users/show.html', basic_information=basic_information, account=account)
 
-
 # アカウント登録画面を表示(=user用)
 @app.route('/users/new')
 def user_new():
   return render_template('users/new.html')
-
 
 # アカウント登録処理(=user用)
 @app.route('/users/create', methods=['POST'])
@@ -112,12 +91,10 @@ def user_create():
   return render_template('users/basic_new.html')
   # return render_template('users/first.html')
 
-
 # 基本情報登録画面を表示
 @app.route('/users/basic_new')
 def user_basic_new():
   return render_template('users/basic_new.html')
-
 
 # 基本情報登録処理
 @app.route('/users/basic_create', methods=['POST'])
@@ -137,43 +114,41 @@ def user_basic_create():
   flash('登録されました', 'success')
   return redirect(url_for('user_login'))
 
-
 # 登録情報修正画面を表示
 @app.route('/<int:account_id>/update')
 @login_check
 def user_edit(account_id):
   account = Account.query.get(account_id)
-  basic_information = basic_information.query.get(account_id)
+  basic_information = Basic_information.query.get(account_id)
   return render_template('users/update.html', account = account, basic_information = basic_information)
-
 
 # 登録情報修正処理
 @app.route('/<int:account_id>/edit', methods=['POST'])
 @login_check
 def user_update(account_id):
   account = Account.query.get(account_id)
-  basic_information = Basic_information(account_id)
-  email = request.form.get('email')
-  password = request.form.get('password')
-  name = request.form.get('name')
-  ruby = request.form.get('ruby')
-  dept = request.form.get('dept')
-  group_name = request.form.get('group_name')
-  year = request.form.get('year')
-  birth_month = request.form.get('birth_month')
-  birth_day = request.form.get('birth_day')
-  team = request.form.get('team')
-  hobby = request.form.get('hobby')
-  word = request.form.get('word')
-  
+  basic_information = Basic_information.query.get(account_id)
+  account.email = request.form.get('email')
+  account.password = request.form.get('password')
+  account.name = request.form.get('name')
+  account.ruby = request.form.get('ruby')
+  account.dept = request.form.get('dept')
+  account.group_name = request.form.get('group_name')
+  account.year = request.form.get('year')
+  basic_information.birth_month = request.form.get('birth_month')
+  basic_information.birth_day = request.form.get('birth_day')
+  basic_information.team = request.form.get('team')
+  basic_information.hobby = request.form.get('hobby')
+  basic_information.word = request.form.get('word')
   try:
-    db.session.merge(item)
+    db.session.merge(account)
+    db.session.merge(basic_information)
     db.session.commit()
   except:
     flash('入力した値を再度確認してください', 'danger')
     return redirect(url_for('user_edit'))
   flash('登録情報が更新されました', 'success')
-  return redirect(url_for('user_show'))
+  return redirect(url_for('user_index'))
 
 @app.template_filter('staticfile')
 def staticfile_filter(fname):
